5a435c6a006e4199c4da58061d92d4b6342efbe3
dimchoo <imdimchoo@gmail.com>
2019-05-17 20:08:38 +0300

    Lesson 1. Task 1.

--- Lesson_1/1.py
+++ Lesson_1/1.py
@@ -1,1 +1,25 @@
-# 1.	Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+# 1. Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+
+user_number = int(input('Введите трехзначное число: '))
+MIN_NUMBER = 100
+MAX_NUMBER = 999
+
+if MIN_NUMBER <= user_number <= MAX_NUMBER:
+    first_number = user_number // 100
+    second_number = (user_number // 10) % 10
+    third_number = user_number % 10
+
+    print(f'Сумма цифр числа: {first_number + second_number + third_number}\n'
+          f'Произведение цифр числа: {first_number * second_number * third_number}')
+
+elif -MIN_NUMBER >= user_number >= -MAX_NUMBER:
+    user_number = abs(user_number)
+    first_number = -(user_number // 100)
+    second_number = (user_number // 10) % 10
+    third_number = user_number % 10
+
+    print(f'Сумма цифр числа: {first_number + second_number + third_number}\n'
+          f'Произведение цифр числа: {first_number * second_number * third_number}')
+
+else:
+    print('Вы ввели некорректные данные!')
