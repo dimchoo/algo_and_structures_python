ab2a9a40f854b69e2a1cb55c3ed9e7797b1c5839
dimchoo <imdimchoo@gmail.com>
2019-05-17 20:11:15 +0300

    Lesson 1. Task 6.

--- Lesson_1/6.py
+++ Lesson_1/6.py
@@ -1,1 +1,12 @@
-# 6.	Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+# 6. Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+
+letter_number = int(input('Введите номер буквы: '))
+
+MIN_LETTER = 1
+MAX_LETTER = 26
+ASCII_COUNT = 96
+
+if MIN_LETTER <= letter_number <= MAX_LETTER:
+    print(f'Под номером "{letter_number}" находится буква "{chr(letter_number + ASCII_COUNT)}"')
+else:
+    print('Некорректный ввод!')
