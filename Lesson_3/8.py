3f9f07df2a02c62db0e76b591dc13d01c1add270
dimchoo <imdimchoo@gmail.com>
2019-05-24 19:42:33 +0300

    Lesson 3. Task 8.

--- Lesson_3/8.py
+++ Lesson_3/8.py
@@ -4,3 +4,21 @@
 записывать ее в последнюю ячейку строки.
 В конце следует вывести полученную матрицу.
 """
+
+ROW_COUNT = 5
+COL_COUNT = 4
+
+matrix = []
+for i in range(ROW_COUNT):
+    row = []
+    sum_row = 0
+    for j in range(COL_COUNT):
+        number = int(input(f'Введите {j + 1} элемент {i + 1} строки: '))
+        sum_row += number
+        row.append(number)
+    row.append(sum_row)
+    matrix.append(row)
+
+for r in matrix:
+    print(r)
+
