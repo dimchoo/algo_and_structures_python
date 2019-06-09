0ee5c54a64bbb47ae3fb6c9809e1d5fa00d3becc
dimchoo <imdimchoo@gmail.com>
2019-06-09 19:13:43 +0300

    Lesson 7. Task 1.

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -10,23 +10,33 @@ import random
 random_list = [random.randint(-100, 100) for _ in range(30)]
 
 
-def bubble_sort(lst):
+def bubble_sort(lst, reverse=True):
     """
-    Функция принимает на вход список с произвольными целыми числами,
-    сортирует его при помощи пузырькового алгоритма (по убыванию)
+    Функция принимает на вход список с произвольными целыми числами
+    и булево значение направления сортировки.
+    Сортирует его при помощи пузырькового алгоритма (по направлению)
     и возвращает отсортированный список
     :param lst: list (unsorted)
+    :param reverse: boolean (default true)
     :return: list (sorted in reverse order)
     """
-
     n = 1
-    while n < len(lst):
-        for i in range(len(lst) - n):
-            if lst[i] < lst[i + 1]:
-                lst[i], lst[i + 1] = lst[i + 1], lst[i]
-        n += 1
-    return lst
+    if reverse:
+        while n < len(lst):
+            for i in range(len(lst) - n):
+                if lst[i] < lst[i + 1]:
+                    lst[i], lst[i + 1] = lst[i + 1], lst[i]
+            n += 1
+        return lst
+    else:
+        while n < len(lst):
+            for i in range(len(lst) - n):
+                if lst[i] > lst[i + 1]:
+                    lst[i], lst[i + 1] = lst[i + 1], lst[i]
+            n += 1
+        return lst
 
 
 print(f'Исходный список: {random_list}')
 print(f'Отсортированный исходный список: {bubble_sort(random_list)}')
+print(f'Отсортированный исходный список: {bubble_sort(random_list, False)}')
