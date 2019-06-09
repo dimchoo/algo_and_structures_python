e4efa9ede69e61922d2dc4c25450e4e51d27c815
dimchoo <imdimchoo@gmail.com>
2019-06-09 19:14:57 +0300

    Lesson 7. Task 2.

--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,45 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+
+import random
+
+random_list = [round(random.uniform(0, 50), 2) for _ in range(10)]
+
+
+def merge_sort(lst):
+    if len(lst) > 1:
+        center = len(lst) // 2
+        left = lst[:center]
+        right = lst[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lst[k] = left[i]
+                i += 1
+            else:
+                lst[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            lst[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            lst[k] = right[j]
+            j += 1
+            k += 1
+        return lst
+
+
+print(f'Исходный список:\n{random_list}')
+print(f'Список, отсортированный слиянием:\n{merge_sort(random_list)}')
+
+
